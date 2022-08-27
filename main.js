499d0beab0e796a4b785f4718dd0d305a975aaf8
Jim <jim@advancebass.com>
2022-08-27 11:07:47 -0700

    add basic three.js start with water shader

--- main.js
+++ main.js
@@ -1,23 +1,77 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import fragmentShader from './fragmentShader';
 
-setupCounter(document.querySelector('#counter'))
+import * as dat from 'dat.gui';
+const app = document.querySelector('#app');
+
+const gui = new dat.GUI();
+const planeRotation = { x: -1.2, y: 0, z: 0 };
+const guiPlane = gui.addFolder('Plane');
+guiPlane.add(planeRotation, 'x', -1.5, 1.5).step(0.1);
+
+// scene
+const scene = new THREE.Scene();
+
+// camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.z = 5;
+
+// box
+const boxGeom = new THREE.BoxGeometry(1, 1, 1);
+const boxMat = new THREE.MeshBasicMaterial({ color: new THREE.Color('coral') });
+const box = new THREE.Mesh(boxGeom, boxMat);
+scene.add(box);
+
+// shader
+const uniforms = {
+  iTime: { value: 0 },
+  iResolution: { value: new THREE.Vector3(planeRotation) },
+};
+
+// plane
+const planeGeom = new THREE.PlaneGeometry(5, 5);
+const planeMat = new THREE.ShaderMaterial({
+  fragmentShader,
+  uniforms,
+});
+
+const plane = new THREE.Mesh(planeGeom, planeMat);
+scene.add(plane);
+
+plane.rotateX(0);
+
+plane.rotation.set(planeRotation.x, planeRotation.y, planeRotation.z);
+console.log(plane.rotation);
+
+// renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+app.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// animation loop
+function animate(time) {
+  time *= 0.001; // convert to seconds
+
+  box.rotation.x += 0.01;
+  box.rotation.y += 0.01;
+
+  plane.rotation.set(planeRotation.x, planeRotation.y, planeRotation.z);
+
+  uniforms.iResolution.value.set(window.innerWidth, window.innerHeight, 1);
+  uniforms.iTime.value = time;
+
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
