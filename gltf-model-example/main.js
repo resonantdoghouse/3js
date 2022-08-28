4d672e85ce0e2417250efd4d6526bb613b307877
Jim <jim@advancebass.com>
2022-08-28 09:28:22 -0700

    refactor to async await

--- gltf-model-example/main.js
+++ gltf-model-example/main.js
@@ -36,11 +36,7 @@ const directionalLightPosition = {
   z: 100,
 };
 
-// let mixer = null;
-let model = null;
-
 const updatePosition = (object, axis, position) => {
-  console.log(object);
   switch (axis) {
     case 'x':
       object.position.x = position;
@@ -56,134 +52,145 @@ const updatePosition = (object, axis, position) => {
   }
 };
 
-// Load Model
-gltfLoader.load('/models/spaceship.gltf', (gltf) => {
-  // console.log('gltf', gltf);
-  model = gltf.scene;
-  model.scale.set(0.5, 0.5, 0.5);
+function modelLoader(url) {
+  return new Promise((resolve, reject) => {
+    gltfLoader.load(url, (data) => resolve(data), null, reject);
+  });
+}
 
+async function loadModel() {
+  const gltf = await modelLoader('/models/spaceship.gltf');
+  const model = gltf.scene;
+  model.scale.set(0.5, 0.5, 0.5);
   model.position.x = modelPosition.x;
   model.position.y = modelPosition.y;
   model.position.z = modelPosition.z;
-
   model.receiveShadow = true;
   model.castShadow = true;
-
   scene.add(model);
 
-  // Floor
-  const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(20, 20),
-    new THREE.MeshStandardMaterial({
-      color: '#444444',
-      metalness: 0.2,
-      roughness: 0.5,
-    })
-  );
-  floor.receiveShadow = true;
-  floor.rotation.x = -Math.PI * 0.5;
-  scene.add(floor);
+  return model;
+}
 
-  // Lights
-  const ambientLight = new THREE.AmbientLight(0xffffff, 3);
-  scene.add(ambientLight);
+const model = await loadModel().catch((error) => {
+  console.error(error);
+});
 
-  const pointLight = new THREE.PointLight(0xffffff, 1);
-  pointLight.castShadow = true;
+// Floor
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(20, 20),
+  new THREE.MeshStandardMaterial({
+    color: '#444444',
+    metalness: 0.2,
+    roughness: 0.5,
+  })
+);
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+// Lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 3);
+scene.add(ambientLight);
 
-  directionalLight.position.set(
-    directionalLightPosition.x,
-    directionalLightPosition.y,
-    directionalLightPosition.z
-  );
+const pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.castShadow = true;
 
-  directionalLight.castShadow = true;
-  directionalLight.shadow.mapSize.set(1024, 1024);
-  directionalLight.shadow.camera.far = 15;
-  directionalLight.shadow.camera.left = -7;
-  directionalLight.shadow.camera.top = 7;
-  directionalLight.shadow.camera.right = 7;
-  directionalLight.shadow.camera.bottom = -7;
-  directionalLight.target = model;
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 
-  scene.add(directionalLight);
+directionalLight.position.set(
+  directionalLightPosition.x,
+  directionalLightPosition.y,
+  directionalLightPosition.z
+);
 
-  // Base camera
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    sizes.width / sizes.height,
-    0.1,
-    100
-  );
-  camera.position.set(0, 2, 4);
-  camera.add(pointLight);
-  scene.add(camera);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.camera.left = -7;
+directionalLight.shadow.camera.top = 7;
+directionalLight.shadow.camera.right = 7;
+directionalLight.shadow.camera.bottom = -7;
+directionalLight.target = model;
 
-  window.addEventListener('resize', () => {
-    // update sizes
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
-    // update camera
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
-    // update renderer
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-  });
+scene.add(directionalLight);
 
-  // Controls
-  const controls = new OrbitControls(camera, canvas);
-  controls.maxPolarAngle = Math.PI / 2; // avoids going below ground
-  controls.target.set(0, 0.75, 0);
-  controls.enableDamping = true;
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(0, 2, 4);
+camera.add(pointLight);
+scene.add(camera);
 
-  const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-  });
-  renderer.shadowMap.enabled = true;
-  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+window.addEventListener('resize', () => {
+  // update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  // update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  // update renderer
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-  // Timing for Animation Loop
-  const clock = new THREE.Clock();
-  let previousTime = 0;
-
-  // Animation Loop, Renderer
-  const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
-    const deltaTime = elapsedTime - previousTime;
-    previousTime = elapsedTime;
-    controls.update();
-    renderer.render(scene, camera);
-    window.requestAnimationFrame(tick);
-  };
-
-  tick();
-
-  /*
-   * Debugging with Dat.GUI
-   */
-  const positionFolder = gui.addFolder('Object Position');
-
-  positionFolder
-    .add(modelPosition, 'x', -10, 10)
-    .step('0.1')
-    .onChange((value) => {
-      updatePosition(model, 'x', value);
-    });
-  positionFolder
-    .add(modelPosition, 'y', 0.5, 3.5)
-    .onChange((value) => {
-      updatePosition(model, 'y', value);
-    })
-    .step('0.1');
-  positionFolder
-    .add(modelPosition, 'z', -10, 10)
-    .onChange((value) => {
-      updatePosition(model, 'z', value);
-    })
-    .step('0.1');
 });
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.maxPolarAngle = Math.PI / 2; // avoids going below ground
+controls.target.set(0, 0.75, 0);
+controls.enableDamping = true;
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Timing for Animation Loop
+const clock = new THREE.Clock();
+let previousTime = 0;
+
+// Animation Loop, Renderer
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+// start loop, if model is loaded
+if (model) {
+  tick();
+}
+
+/*
+ * Debugging with Dat.GUI
+ */
+const positionFolder = gui.addFolder('Object Position');
+
+positionFolder
+  .add(modelPosition, 'x', -10, 10)
+  .step('0.1')
+  .onChange((value) => {
+    updatePosition(model, 'x', value);
+  });
+positionFolder
+  .add(modelPosition, 'y', 0.5, 3.5)
+  .onChange((value) => {
+    updatePosition(model, 'y', value);
+  })
+  .step('0.1');
+positionFolder
+  .add(modelPosition, 'z', -10, 10)
+  .onChange((value) => {
+    updatePosition(model, 'z', value);
+  })
+  .step('0.1');
